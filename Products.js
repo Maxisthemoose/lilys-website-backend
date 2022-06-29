c7429225d885367997e84e580ed817e558483df5
Maxisthemoose <oriontothemax@gmail.com>
2022-06-29 14:54:37 -0700

    update products

--- Products.js
+++ Products.js
@@ -2,15 +2,25 @@ module.exports.products = {
   prints: [
     {
       image: "../images/placeholder.jpg",
-      description: "Lorem Ipsum",
-      name: "Lorem Ipsum",
+      description: "Painted in some medium on LxW canvas",
+      name: "Some Name For Art",
       price: 20.00
     },
   ],
   clothing: [
-
+    {
+      image: "../images/placeholder.jpg",
+      description: "Hand painted with some medium on high quality fabric",
+      name: "Some Name For Art/Cloth",
+      price: 30.00
+    },
   ],
   stickers: [
-
+    {
+      image: "../images/placeholder.jpg",
+      description: "Painted with some medium and laminated",
+      name: "Some Name For Sticker",
+      price: 5.00
+    },
   ]
 }
