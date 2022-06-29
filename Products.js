87cd6bcfebf35083505419aeba55e340fa2151be
Maxisthemoose <oriontothemax@gmail.com>
2022-06-29 16:50:15 -0700

    add new products

--- Products.js
+++ Products.js
@@ -6,12 +6,24 @@ module.exports.products = {
       name: "Some Name For Art",
       price: 20.00
     },
+    {
+      image: "../images/placeholder.jpg",
+      description: "Painted in some medium on LxW canvas",
+      name: "Some Other Piece of Art",
+      price: 20.00
+    },
   ],
   clothing: [
     {
       image: "../images/placeholder.jpg",
       description: "Hand painted with some medium on high quality fabric",
-      name: "Some Name For Art/Cloth",
+      name: "Some Name For Art Clothing",
+      price: 30.00
+    },
+    {
+      image: "../images/placeholder.jpg",
+      description: "Hand painted with some medium on high quality fabric",
+      name: "Some Other Art Clothing",
       price: 30.00
     },
   ],
@@ -19,8 +31,8 @@ module.exports.products = {
     {
       image: "../images/placeholder.jpg",
       description: "Painted with some medium and laminated",
-      name: "Some Name For Sticker",
-      price: 5.00
+      name: "Some Other Sticker",
+      price: 7.00
     },
   ]
 }
